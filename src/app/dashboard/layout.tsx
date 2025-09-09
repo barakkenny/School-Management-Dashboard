c47a7ac540db9e73403568a7820764d1f0297173
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-09-09 23:49:35 +0100

    Added icons to the dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Menu from "@/components/Menu"
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-      <div className="h-screen flex gap-10">
-      <div className="w-[14%] bg-red-50">
-        l
+      <div className="h-screen flex">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+        <Link href="/" className='flex items-center justify-center lg:justify-start gap-2'>
+        <Image src="/logo" alt="logo" width={32} height={32}/>
+        <span className='hidden lg:block'>School</span>
+        </Link>
+        <Menu />
       </div>
 
-      <div className="w-[86%] bg-blue-200">r</div>
+      <div className="w-[86%] md:-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
       </div>
   );
 }
